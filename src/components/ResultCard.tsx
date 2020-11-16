61761df2d72cfbb493f10c02ca9b1a03f3735e51
JohnIrle <irlejohn@gmail.com>
2020-11-15 20:35:12 -0600

    Move style to module

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import styles from "./ResultCard.module.css";
 
 interface ResultCardProps {
-    title: string;
-    extract: string;
-    url: string;
+  title: string;
+  extract: string;
+  url: string;
 }
 
 const ResultCard: React.FC<ResultCardProps> = ({ title, extract, url }) => {
-    return (
-        <Card style={{ paddingBottom: "1rem" }}>
-            <Card.Body>
-                <Card.Title>
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                        {title}
-                    </a>
-                </Card.Title>
-                <Card.Text>{extract}</Card.Text>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card className={styles.card}>
+      <Card.Body>
+        <Card.Title>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </Card.Title>
+        <Card.Text>{extract}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default ResultCard;
