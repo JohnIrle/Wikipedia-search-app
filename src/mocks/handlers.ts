02a05ca5c14f41af64714ca0a7162567bd701224
John Irle <irlejohn@gmail.com>
2022-02-22 23:56:35 -0600

    test: add searchform tests.

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
-import { rest } from 'msw';
+import { rest } from "msw";
 
 export const handlers = [
-  rest.get('https://en.wikipedia.org/w/api.php', (req, res, ctx) => {
+  rest.get("https://en.wikipedia.org/w/api.php", (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
@@ -11,7 +11,8 @@ export const handlers = [
             ns: 0,
             title: "Intelligence quotient",
             index: 10,
-            extract: "An intelligence quotient (IQ) is a total score derived from a set of standardized tests or subtests designed to assess human intelligence.",
+            extract:
+              "An intelligence quotient (IQ) is a total score derived from a set of standardized tests or subtests designed to assess human intelligence.",
             contentmodel: "wikitext",
             pagelanguage: "en",
             pagelanguagehtmlcode: "en",
@@ -20,21 +21,24 @@ export const handlers = [
             lastrevid: 1070670945,
             length: 160964,
             fullurl: "https://en.wikipedia.org/wiki/Intelligence_quotient",
-            editurl: "https://en.wikipedia.org/w/index.php?title=Intelligence_quotient&action=edit",
+            editurl:
+              "https://en.wikipedia.org/w/index.php?title=Intelligence_quotient&action=edit",
             canonicalurl: "https://en.wikipedia.org/wiki/Intelligence_quotient",
             thumbnail: {
-              source: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Raven_Matrix.svg/100px-Raven_Matrix.svg.png",
+              source:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Raven_Matrix.svg/100px-Raven_Matrix.svg.png",
               width: 100,
-              height: 100
+              height: 100,
             },
-            pageimage: "Raven_Matrix.svg"
+            pageimage: "Raven_Matrix.svg",
           },
           29090: {
             pageid: 29090,
             ns: 0,
             title: "Software testing",
             index: 3,
-            extract: "Software testing is the act of examining the artifacts and the behavior of the software under test by validation and verification.",
+            extract:
+              "Software testing is the act of examining the artifacts and the behavior of the software under test by validation and verification.",
             contentmodel: "wikitext",
             pagelanguage: "en",
             pagelanguagehtmlcode: "en",
@@ -43,11 +47,12 @@ export const handlers = [
             lastrevid: 1072404730,
             length: 95358,
             fullurl: "https://en.wikipedia.org/wiki/Software_testing",
-            editurl: "https://en.wikipedia.org/w/index.php?title=Software_testing&action=edit",
-            canonicalurl: "https://en.wikipedia.org/wiki/Software_testing"
+            editurl:
+              "https://en.wikipedia.org/w/index.php?title=Software_testing&action=edit",
+            canonicalurl: "https://en.wikipedia.org/wiki/Software_testing",
           },
-        }
+        },
       })
-    )
-  })
-]
+    );
+  }),
+];
---
+++ src/test/SearchForm.test.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { render } from "@testing-library/react";
+import SearchForm from "../components/SearchForm";
+import userEvent from "@testing-library/user-event";
+
+describe("SearchForm", () => {
+  it("renders", () => {
+    const onSubmit = jest.fn();
+    render(<SearchForm submit={onSubmit} />);
+  });
+
+  it("disables button when search is empty", () => {
+    const onSubmit = jest.fn();
+    const { getByText } = render(<SearchForm submit={onSubmit} />);
+
+    expect(getByText("Submit")).toBeDisabled();
+  });
+
+  it("calls submit function", async () => {
+    const onSubmit = jest.fn(e => e.preventDefault());
+    const { getByText, getByPlaceholderText } = render(
+      <SearchForm submit={onSubmit} />
+    );
+
+    await userEvent.type(
+      getByPlaceholderText("Enter a term to search."),
+      "test"
+    );
+    userEvent.click(getByText("Submit"));
+
+    expect(onSubmit).toHaveBeenCalled();
+  });
+});
